b2b1d5b7a4607426279483d9340c55ae604da3c3
renzoyzz <renzoyzevallos@gmail.com>
2021-09-03 13:10:51 -0700

    create directory, add health check

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,8 @@
 FROM node:alpine
 RUN apk add  --no-cache ffmpeg
 WORKDIR /app
-COPY package*.json .
+COPY package*.json ./
 RUN npm install
-COPY . .
+COPY . ./
 ENTRYPOINT ["node", "index.js"]
+EXPOSE 8080
--- index.js
+++ index.js
@@ -2,8 +2,26 @@ require("dotenv").config();
 const { Client, Intents } = require("discord.js");
 const { DownloaderHelper } = require("node-downloader-helper");
 const fs = require("fs");
+const { join } = require("path");
+const contentPath = join(__dirname, `content`);
+if (!fs.existsSync(contentPath)) {
+  fs.mkdirSync(contentPath);
+}
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const express = require("express");
+const app = express();
+app.listen(8080);
+app.get("/", async (req, res) => {
+  // await new Promise((resolve) => {
+  //   setTimeout(() => {
+  //     resolve();
+  //   }, 20000);
+  // });
+  res.send("healthy");
+});
+
 let guildQueues = new Map();
+let guildConnections = new Map();
 
 class Queue {
   constructor() {
@@ -38,7 +56,10 @@ class Queue {
         let current = this.dequeue();
         let connection = await current.channel.join();
         connection.setMaxListeners(0);
-        let dispatcher = connection.play(`./content/${current.username}.wav`);
+        guildConnections.set(current.channel.guild.id, connection);
+        let dispatcher = connection.play(
+          join(contentPath, `${current.username}.wav`)
+        );
         await new Promise((resolve) => {
           dispatcher.once("finish", (end) => {
             resolve();
@@ -64,17 +85,27 @@ client.on("voiceStateUpdate", async (oldVoiceState, newVoiceState) => {
     }
     let queue = guildQueues.get(guildId);
 
-    if (!fs.existsSync(`./content/${member.displayName}.wav`)) {
+    if (!fs.existsSync(join(__dirname, `content/${member.displayName}.wav`))) {
       let url = `https://tetyys.com/SAPI4/SAPI4?text=Welcome%20${encodeURI(
         member.displayName
       )}%20to%20the%20big%20dick%20club&voice=Mike%20in%20Hall&pitch=100&speed=140`;
-      let dl = new DownloaderHelper(url, "content", {
+      let dl = new DownloaderHelper(url, contentPath, {
         fileName: `${member.displayName}.wav`,
       });
       await dl.start();
     }
     queue.enqueue(member.displayName, channel);
     queue.playQueue();
+  } else if (!newVoiceState.channel) {
+    let channel = oldVoiceState.channel;
+    let guildId = channel.guild.id;
+    if (channel.members.size <= 1) {
+      guildQueues.delete(guildId);
+      if (guildConnections.has(guildId)) {
+        guildConnections.get(guildId).disconnect();
+        guildConnections.delete(guildId);
+      }
+    }
   }
 });
 
